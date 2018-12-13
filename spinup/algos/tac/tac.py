c08678615c478dc4215d732b807855bacc91d64c
kyungjaelee <dlxhrl@snu.ac.kr>
2018-12-13 15:59:40 +0900

    Fix tac.py

--- spinup/algos/tac/tac.py
+++ spinup/algos/tac/tac.py
@@ -320,7 +320,7 @@ if __name__ == '__main__':
     parser.add_argument('--l', type=int, default=1)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--exp_name', type=str, default='tac')
     parser.add_argument('--q', type=float, default=1.0)
     parser.add_argument('--alpha', type=float, default=0.2)
