3f47782c0d61ed9c4e02c0981c087667bff75188
kyungjaelee <dlxhrl@snu.ac.kr>
2019-01-10 02:49:25 +0900

    Change sac hyperparams

--- spinup/algos/sac/sac.py
+++ spinup/algos/sac/sac.py
@@ -320,8 +320,8 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='HalfCheetah-v2')
-    parser.add_argument('--hid', type=int, default=200)
-    parser.add_argument('--l', type=int, default=2)
+    parser.add_argument('--hid', type=int, default=300)
+    parser.add_argument('--l', type=int, default=1)
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--exp_name', type=str, default='sac')
