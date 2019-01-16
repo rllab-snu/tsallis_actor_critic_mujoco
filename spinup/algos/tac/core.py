7b0308ad177ec4861c7502ebc65ce9b0792eaecf
kyungjaelee <dlxhrl@snu.ac.kr>
2019-01-16 10:38:56 +0900

    Fix bug in clipping pdf

--- spinup/algos/tac/core.py
+++ spinup/algos/tac/core.py
@@ -60,7 +60,7 @@ def mlp_q_gaussian_policy(x, a, q_prime, hidden_sizes, activation, output_activa
         squashed_mu = tf.tanh(mu)
         squashed_pi = tf.tanh(pi)
         logp_pi = -0.5 * (((pi-mu)/(tf.exp(log_std)+EPS))**2 + 2*log_std + np.log(2*np.pi)) - tf.log(clip_but_pass_gradient(1 - squashed_pi**2, l=0, u=1)+1e-6)
-        q_logp_pi = tf.reduce_sum(tf_log_q(tf.maximum(tf.exp(logp_pi),tf.pow(10.,8/(1-q))),q), axis=1)
+        q_logp_pi = tf.reduce_sum(tf_log_q(tf.minimum(tf.exp(logp_pi),tf.pow(10.,8/(1-q))),q), axis=1)
         
     if pdf_type=="gaussian" and log_type=="scaled-q-log":
         pi = mu + tf.random_normal(tf.shape(mu)) * std
@@ -68,7 +68,7 @@ def mlp_q_gaussian_policy(x, a, q_prime, hidden_sizes, activation, output_activa
         squashed_pi = tf.tanh(pi)
         #logp_pi = gaussian_likelihood(pi, mu, log_std) - tf.reduce_sum(tf.log(clip_but_pass_gradient(1 - squashed_pi**2, l=1e-5, u=1)), axis=1)
         logp_pi = -0.5 * (((pi-mu)/(tf.exp(log_std)+EPS))**2 + 2*log_std + np.log(2*np.pi)) - tf.log(clip_but_pass_gradient(1 - squashed_pi**2, l=0, u=1)+1e-6)
-        q_logp_pi = tf.reduce_sum(tf.maximum(tf_log_q(tf.exp(logp_pi),q)*tf.log(1/2.0)/tf_log_q(1/2.0,q),1e7), axis=1) 
+        q_logp_pi = tf.reduce_sum(tf.minimum(tf_log_q(tf.exp(logp_pi),q)*tf.log(1/2.0)/tf_log_q(1/2.0,q),1e7), axis=1) 
         
     elif pdf_type=="q-gaussian" and log_type=="q-log":
         pi = mu + tf_random_q_normal(tf.shape(mu),q) * std
